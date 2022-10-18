c9228e7611ca4dc30a076dbfaa2537fd6b9a51d3
Kyle Guarco <kyleguarco55@gmail.com>
2022-10-18 18:38:16 -0400

    Initial Commit

---
+++ Error.java
@@ -0,0 +1,9 @@
+package com.github.fdx;
+
+public class Error {
+	public Error(String message, int line) {
+		System.out.println("Line " + line + ": " + message);
+		System.exit(0);
+	}
+}
+
---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.github.fdx;
+
+public class Main {
+	public static void main(String[] args) {
+		Parser p = new Parser();
+		p.parse();
+		System.out.println("The syntax of the source program is correct.");
+	}
+}
+
